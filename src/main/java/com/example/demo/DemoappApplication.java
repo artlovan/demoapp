84956a21af5650a8bea57d0379d2e7934b944f87
artlovan <ijavadriven@gmail.com>
2018-04-28 22:22:52 -0400

    change

--- src/main/java/com/example/demo/DemoappApplication.java
+++ src/main/java/com/example/demo/DemoappApplication.java
@@ -19,6 +19,6 @@ public class DemoappApplication {
 
 	@GetMapping(value = "get", produces = APPLICATION_JSON_VALUE)
 	public ResponseEntity<?> get() {
-		return new ResponseEntity<>("{\"msg\": \"Spinnaker!\"}", HttpStatus.OK);
+		return new ResponseEntity<>("{\"msg\": \"rocks!\"}", HttpStatus.OK);
 	}
 }
